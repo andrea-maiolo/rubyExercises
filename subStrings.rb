d203343ecf49746d2e6e773d632fea2925b0c29b
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-10-19 18:10:50 +0200

    subStrings done

--- subStrings.rb
+++ subStrings.rb
@@ -5,20 +5,29 @@
 
 
 def substring(string,array)
+  #this make thestring easy to work with
   string = string.downcase
+  #create a new empty hash
   obj = Hash.new
+  #for every ele in array put it in a regex and check if match the string
   for ele in array do
     regex = /#{ele}/
-    counting = 3
-    if string.match(regex)
-      obj[ele] = string.match(regex).size
+    if string.scan(regex)   
+      obj[ele]= string.scan(regex)
+    end
+  end
+  obj.each do |key,value|
+    c =0 
+    for e in value do
+      c +=1
+    end
+    obj[key] = c
+    if obj[key] == 0
+      obj.delete(key)
     end
   end
   puts obj
 end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-#substring("below", dictionary)
-
-
+#substring("iii", dictionary)
 substring("Howdy partner, sit down! How's it going?", dictionary)
-#{ "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
