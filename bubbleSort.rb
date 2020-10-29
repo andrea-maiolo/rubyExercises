f993e5a2122bf9fc9ddc5854f7807393dc153d4b
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-10-29 15:22:44 +0100

    bubbleSort algorithm done

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -1,25 +1,23 @@
 # Build a method #bubble_sort that takes an array and returns a sorted array. It must use the bubble sort methodology (using #sort would be pretty pointless, wouldn’t it?).
 # > bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
+ 
 
 def bubbleSort(array)
-    n = array.length
-    loop do
-      m = 0
-      for i =1 to n - 1 .. do
-        if array[i - 1] > array[i] 
-            
+  sorted = array.sort
+  if sorted == array 
+    p array 
+    exit
+  end
+  array.each do |e|
+    eI = array.index(e)
+    eN = eI +1 
+    n =  array.at(eN)
+    if n == nil then bubbleSort(array) end
+    if e > n 
+      array[eI], array[eN] = array[eN], array[eI]
     end
-    # repeat
-    #     newn := 0
-    #     for i := 1 to n - 1 inclusive do
-    #         if A[i - 1] > A[i] then
-    #             swap(A[i - 1], A[i])
-    #             newn := i
-    #         end if
-    #     end for
-    #     n := newn
-    # until n ≤ 1
-end 
+  end
+end
 
 bubbleSort([4,3,78,2,0,2])
